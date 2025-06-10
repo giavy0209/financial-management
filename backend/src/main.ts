8c93b42972a6e6d452036c8034ad53cb9019d63d
vy.pham <49144082+giavy0209@users.noreply.github.com>
2025-06-10 08:25:13 +0700

    test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,10 +7,12 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   // Enable CORS for all origins
   app.enableCors({
-    origin: '*',
+    origin: '*', // Allow requests from any domain
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
 
   await app.listen(process.env.PORT ?? 3001);
